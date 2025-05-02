d8cd4c2a9e1e131f2336a2929c0085ffa6a17d32
sonya avramenko <sofijaavramenko@gmail.com>
2025-05-02 19:09:24 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"sum.h"
+using namespace std;
+
+int main
+{
+    int a = 5, b = 2;
+    cout<<"Hello world;
+    cout<<a<<" + "<<b<<" = "<<sum(a,b)<<endl;
+}
---
+++ quot.cpp
@@ -0,0 +1,5 @@
+#incude"sum.h"
+int quot(int a , int b);
+{
+   return a/b;
+}
